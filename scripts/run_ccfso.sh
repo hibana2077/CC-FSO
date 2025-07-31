e20a4a22c311e5faf423e3013b3b33ece252ed35
hibana2077 <hibana2077@gmail.com>
2025-07-31 16:36:07 +0800

    fix: Update output file name in run_ccfso_v100.sh to out_v100.txt

--- scripts/run_ccfso.sh
+++ scripts/run_ccfso.sh
@@ -16,5 +16,5 @@ nvidia-smi >> gpu-info.txt
 cd ..
 
 source /scratch/rp06/sl5952/CC-FSO/.venv/bin/activate
-python3 train.py --dataset cotton80 --model resnet152 --loss ce --optimizer riemannian_sgd --epochs 100 --batch_size 32 --lr 0.001 >> out_v100.txt
+python3 train.py --dataset cotton80 --model resnet152 --loss ce --optimizer riemannian_sgd --epochs 100 --batch_size 32 --lr 0.001 >> out_a100.txt
 
